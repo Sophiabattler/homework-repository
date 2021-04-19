84775eb842cb3c1eea80fe40bfa9c72a7014ac9c
Sophiabattler <79052071542@yandex.ru>
2021-04-19 15:26:45 +0300

    Changes in paths

--- tests/homework1/test_task03.py
+++ tests/homework1/test_task03.py
@@ -9,8 +9,7 @@ def test_min_max():
     as a tuple"""
     assert (
         find_maximum_and_minimum(
-            r"C:\Users\User\EPAM\
-    homework-repository\homework1\test_task3_1.txt"
+            r"C:\Users\User\EPAM\homework-repository\homework1\test_task3_1.txt"
         )
         == (-2000, 2000)
     )
@@ -22,8 +21,7 @@ def test_with_one_line():
     as a tuple for one line"""
     assert (
         find_maximum_and_minimum(
-            r"C:\Users\User\EPAM\
-    homework-repository\homework1\test_task3_2.txt"
+            r"C:\Users\User\EPAM\homework-repository\homework1\test_task3_2.txt"
         )
         == (5, 5)
     )
