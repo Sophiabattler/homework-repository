af384031fed4e1cc28842637d84bdea8fb8a5f8e
Sophiabattler <79052071542@yandex.ru>
2021-04-19 16:11:14 +0300

    changed '\' to '/'

--- tests/homework1/test_task03.py
+++ tests/homework1/test_task03.py
@@ -7,11 +7,11 @@ def test_min_max():
     """Testing that file.txt will be read
     and min and max values will be returned
     as a tuple"""
-    assert find_maximum_and_minimum(r"homework1\test_task3_1.txt") == (-2000, 2000)
+    assert find_maximum_and_minimum(r"./homework1/test_task3_1.txt") == (-2000, 2000)
 
 
 def test_with_one_line():
     """Testing that file.txt will be read
     and min and max values will be returned
     as a tuple for one line"""
-    assert find_maximum_and_minimum(r"homework1\test_task3_2.txt") == (5, 5)
+    assert find_maximum_and_minimum(r"./homework1/test_task3_2.txt") == (5, 5)
