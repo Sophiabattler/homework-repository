20d347be0d861714520b1f729411d15c14612f0c
Sophiabattler <79052071542@yandex.ru>
2021-05-06 18:36:37 +0300

    update black

--- homework1/sample_project/calculator/calc.py
+++ homework1/sample_project/calculator/calc.py
@@ -2,7 +2,7 @@
 
 
 def check_power_of_2(arg: int) -> bool:
-    """Checking if a number is a power of two """
+    """Checking if a number is a power of two"""
     if arg != 0:
         return not bool(arg & (arg - 1))
     return False
