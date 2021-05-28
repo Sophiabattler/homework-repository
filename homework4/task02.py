f8cde4b83fcb0c290196b616ebf61e87eacb3dbe
Sophiabattler <79052071542@yandex.ru>
2021-05-28 13:45:11 +0300

    changes in task02 -url

--- homework4/task02.py
+++ homework4/task02.py
@@ -1,12 +1,12 @@
-"""Task02 - URL"""
-from urllib import request
+"""Task02_var1 - URL"""
+from urllib.request import urlopen
 
 
-def count_dots_on_i(url: str) -> int:
+def count_dots_on_i1(url: str) -> int:
     """Count how many letters `i` are present in the HTML by this URL"""
     try:
-        response = request.urlopen(url)
+        response = urlopen(url)
     except ConnectionError:
-        raise ValueError("Unreachable {url}") from None
+        raise ValueError("Unreachable {url}")
     data = response.read()
     return data.count(b"i")
