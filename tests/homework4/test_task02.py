efbe7b8b9c68d6d48bc1bc6dba86bac605c971e8
Sophiabattler <79052071542@yandex.ru>
2021-05-28 13:49:43 +0300

    changes in test

--- tests/homework4/test_task02.py
+++ tests/homework4/test_task02.py
@@ -3,20 +3,20 @@ from unittest import TestCase
 from unittest.mock import MagicMock, Mock, patch
 from urllib import request
 
-from homework4 import task02_var1
-from homework4.task02_var1 import count_dots_on_i1
+from homework4 import task02
+from homework4.task02 import count_dots_on_i1
 
 
 class TestFunc1(TestCase):
     def test_count_i1_where_i_exists(self):
-        with patch.object(task02_var1, "urlopen", return_value=Mock()) as urlopen_mock:
+        with patch.object(task02, "urlopen", return_value=Mock()) as urlopen_mock:
             mock_response_read = Mock()
             urlopen_mock.return_value.read = mock_response_read
             mock_response_read.return_value = b"iii"
             assert count_dots_on_i1("test_get_data") == 3
 
     def test_count_i1_where_i_does_not_exist(self):
-        with patch.object(task02_var1, "urlopen", return_value=Mock()) as urlopen_mock:
+        with patch.object(task02, "urlopen", return_value=Mock()) as urlopen_mock:
             mock_response_read = Mock()
             urlopen_mock.return_value.read = mock_response_read
             mock_response_read.return_value = b"not"
