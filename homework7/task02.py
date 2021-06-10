dd949c4d73afcd16b83f4ce8d6a7a228b2f9ee08
Sophiabattler <79052071542@yandex.ru>
2021-06-11 01:41:28 +0300

    changes in homework7.task02

--- homework7/task02.py
+++ homework7/task02.py
@@ -8,17 +8,13 @@ def backspace_compare(first: str, second: str):
         """Returns new word without # and erased letters"""
 
         new_word = []
-        if len(word) > 1 and word[0] != "#" and word[1] != "#":
-            new_word.append(word[0])
-        for i in reversed(range(1, len(word) - 1)):
-            if (word[i] == "#" and (word[i - 1] != "#" or word[i - 1] == "#")) or (
-                word[i] != "#" and word[i + 1] == "#"
-            ):
-                continue
+        for i in word:
+            if i != "#":
+                new_word.append(i)
+            elif i == "#" and len(new_word) == 0:
+                pass
             else:
-                new_word.append(word[i])
-        if len(word) > 0 and word[len(word) - 1] != "#":
-            new_word.append(word[len(word) - 1])
+                new_word.pop()
         return new_word
 
     if counter_backspace(first) != counter_backspace(second):
